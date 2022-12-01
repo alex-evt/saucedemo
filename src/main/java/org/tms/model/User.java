759feac1679f0b5f06a1f72ffa1e173babdfde1b
Alex <Alexander.evtukh@yandex.ru>
2022-12-01 19:35:00 +0300

    fixed

--- src/main/java/org/tms/model/User.java
+++ src/main/java/org/tms/model/User.java
@@ -21,19 +21,4 @@ public class User {
         this.password = password;
     }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
